0512b914453e0648dc5ad73b10d032e3667b9387
leFos95 <lelos_soulis@yahoo.gr>
2017-02-05 16:08:11 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,77 @@
-var spot = {
-  x: 100,
-  y: 50
-};
+var balls = [];
+var hoops = [];
 
-var col = {
-  r: 255,
-  g: 0,
-  b: 0
-};
+
 
 function setup() {
-  createCanvas(600, 400);
-  background(0);
+  createCanvas(800, 600);
+  hoops.push(new Hoop());
   
 }
 
+function mousePressed() {
+  var b = new Ball(mouseX, mouseY);
+  balls.push(b);
+}
+
 function draw() {
-  col.r = random(100, 255);
-  col.g = 0;
-  col.b = random(100, 190);
-  spot.x = random(0, width);
-  spot.y = random(0, height);
-  fill(col.r, col.g, col.b);
-  ellipse(spot.x, spot.y, 24, 24);
+  background(0);
+  for (var j = 0; j < hoops.length; j++){
+    hoops[j].show();
+     if (hoops[j] <= 1){
+       hoops.push(new Hoop());
+    }
+   
+  }
+  for (var i = 0; i < balls.length; i++){
+    balls[i].display();
+    balls[i].move();
+    balls[i].bounce();
+    if(balls[i].isDone()){
+      balls.splice(i, 1);
+    }
+  }
+}
+  
+function Hoop(){
+  this.x = random(0, width);
+  this.y = random(0, height);
+  
+  this.show = function() {
+    stroke(255);
+    noFill();
+    ellipse(this.x, this.y, 48, 48);
+  }
+  
+}
+
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.upspeed = 3;
+  this.fade = 255;
+  
+  this.display = function() {
+    fill(255, this.fade);
+    ellipse(this.x, this.y, 20, 20);
+  }
+  
+  this.isDone = function() {
+    if (this.fade < 0){
+      return true;
+    } else {
+      return false;
+    }
+  } 
+  
+  this.move = function() {
+    this.y = this.y + this.upspeed;
+    this.fade = this.fade - 1;
+  }
+  
+  this.bounce = function() {
+    if(this.y > height || this.y < 0) {
+      this.upspeed = this.upspeed * -1;
+    }
+  }
 }
